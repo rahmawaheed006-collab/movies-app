a496bd92d68e4390d68109faf7f4e41d7ea5f3e1
Rahma Waheed <rahmawaheed006@gmail.com>
2026-09-17 05:22:28 +0300

    pdate hase 1 screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // ده الملف اللي flutterfire configure عمله تلقائيًا
+import 'firebase_options.dart';
 import 'core/theme/app_theme.dart';
 import 'features/splash/splash_screen.dart';
 
 Future<void> main() async {
-  // لازم السطر ده قبل أي حاجة تانية علشان تقدر تستخدم Firebase
+
   WidgetsFlutterBinding.ensureInitialized();
 
-  // بيشغل الاتصال بمشروع Firebase بتاعك باستخدام الإعدادات
-  // اللي اتولدت في firebase_options.dart
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
